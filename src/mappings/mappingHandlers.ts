31ed1c4e51d7312b558b42188e3a4ed16871ac5d
loungie <2640297@mail.ru>
2021-11-23 11:45:42 +0000

    create project

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,15 @@
+import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
+import {StarterEntity} from "../types";
+import {Balance} from "@polkadot/types/interfaces";
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+    const {event: {data: [account, balance]}} = event;
+    //Retrieve the record by its ID
+    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
+    record.field2 = account.toString();
+    //Big integer type Balance of a transfer event
+    record.field3 = (balance as Balance).toBigInt();
+    await record.save();
+}
+
+
